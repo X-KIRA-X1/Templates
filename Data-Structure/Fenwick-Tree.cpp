e2bcfe5a8792ec205e3f25152e1f726a8b31aff0
Mostafa Mahanna <mostafamahana893@gmail.com>
2026-04-23 06:29:31 +0200

    Fenwicks added
    Implement a Fenwick Tree (Binary Indexed Tree) for efficient prefix sum queries and updates.

---
+++ Data-Structure/Fenwick-Tree.cpp
@@ -0,0 +1,48 @@
+struct fenwickTree
+{
+    int n;
+    vector <int> data;
+
+    fenwickTree(int size)
+    {
+        n = 1;
+        while (n < size) n <<= 1;
+        data.resize(n + 1 , 0);
+    }
+
+    void add (int i , int v)
+    {
+        while (i <= n)
+        {
+            data[i] += v;
+            i += (i & -i);
+        }
+    }
+
+    int get (int i)
+    {
+        int res = 0;
+        while (i > 0)
+        {
+            res += data[i];
+            i -= (i & -i);
+        }
+
+        return res;
+    }
+
+    int lowerBound (int sum)
+    {
+        int i = 0;
+        for (int step = n ; step > 0 ; step >>= 1)
+        {
+            if (data[i + step] < sum)
+            {
+                sum -= data[i + step];
+                i += step;
+            }
+        }
+
+        return i + 1;
+    }
+};
